528aa356dd4a652ad6ea9ccd8bdb4de3d93ef961
xug15 <m13001271022@163.com>
2025-04-21 11:35:59 +0800

    first commit

---
+++ upload.sh
@@ -0,0 +1,12 @@
+label=`data`
+echo ${label}
+git commit -m "first commit"
+git add *
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:xug15/janus_protein.git
+git push -u origin main
+
+
+
+
